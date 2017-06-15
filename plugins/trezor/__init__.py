f6a3dbc59fcddffd6e0e1c38678f26968e21ae6a
Jack Robison <jackrobison@lbry.io>
2017-06-15 15:12:27 -0400

    delete lbryum/plugins

--- plugins/trezor/__init__.py
+++
@@ -1,9 +0,0 @@
-from lbryum.i18n import _
-
-fullname = 'Trezor Wallet'
-description = _('Provides support for Trezor hardware wallet')
-requires = [('trezorlib','github.com/trezor/python-trezor')]
-requires_wallet_type = ['trezor']
-registers_wallet_type = ('hardware', 'trezor', _("Trezor wallet"))
-available_for = ['qt', 'cmdline']
-
