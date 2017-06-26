b1a5f625f35168c9dca181aa0ae30c3aa15e6e58
Jack Robison <jackrobison@lbry.io>
2017-06-26 13:33:27 -0400

    update __init__.py, add __version__

--- lbryum/__init__.py
+++ lbryum/__init__.py
@@ -1,12 +1,7 @@
-import account
-import lbrycrd
-import transaction
-from coinchooser import COIN_CHOOSERS
-from commands import Commands, known_commands
-from interface import Connection, Interface
-from network import DEFAULT_PORTS, Network, pick_random_server
-from simple_config import SimpleConfig, get_config, set_config
-from transaction import Transaction
-from util import format_satoshis, print_error, print_msg, set_verbosity
-from version import LBRYUM_VERSION
-from wallet import Imported_Wallet, Synchronizer, Wallet, WalletStorage
+import logging
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.INFO)
+
+__version__ = '3.0.1'
