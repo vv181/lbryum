36fd5cf9cd1696705c75acc25c8f0a1eb8541c1e
Jack Robison <jackrobison@lbry.io>
2017-07-18 17:44:33 -0400

    redundant logging handler

--- lbryum/__init__.py
+++ lbryum/__init__.py
@@ -4,7 +4,7 @@ log = logging.getLogger("lbryum")
 
 DEFAULT_FORMAT = "%(asctime)s %(levelname)-8s %(name)s:%(lineno)d: %(message)s"
 
-handler = logging.StreamHandler()
+handler = logging.NullHandler()
 handler.setFormatter(logging.Formatter(DEFAULT_FORMAT))
 log.addHandler(handler)
 log.setLevel(logging.INFO)
